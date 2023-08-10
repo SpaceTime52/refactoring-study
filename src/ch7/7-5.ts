b4e7af0717c0769b1102272bc4c0e750da8f0869
SpaceTime52 <love.free.work@gmail.com>
2023-08-10 10:37:46 +0900

    feat: 7.5 클래스 추출하기

--- src/ch7/7-5.ts
+++ src/ch7/7-5.ts
@@ -1,12 +1,40 @@
+class TelephoneNumber {
+  #areaCode: string;
+  #number: string;
+
+  constructor(areaCode: string, number: string) {
+    this.#areaCode = areaCode;
+    this.#number = number;
+  }
+
+  get areaCode(): string {
+    return this.#areaCode;
+  }
+
+  set areaCode(arg: string) {
+    this.#areaCode = arg;
+  }
+
+  get number(): string {
+    return this.#number;
+  }
+
+  set number(arg: string) {
+    this.#number = arg;
+  }
+
+  get toString(): string {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+}
+
 class Person {
   #name: string;
-  #officeAreaCode: string;
-  #officeNumber: string;
+  #telephoneNumber: TelephoneNumber;
 
   constructor(name: string, areaCode: string, number: string) {
     this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+    this.#telephoneNumber = new TelephoneNumber(areaCode, number);
   }
 
   get name(): string {
@@ -18,23 +46,23 @@ class Person {
   }
 
   get telephoneNumber(): string {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    return this.#telephoneNumber.toString;
   }
 
   get officeAreaCode(): string {
-    return this.#officeAreaCode;
+    return this.#telephoneNumber.areaCode;
   }
 
   set officeAreaCode(arg: string) {
-    this.#officeAreaCode = arg;
+    this.#telephoneNumber.areaCode = arg;
   }
 
   get officeNumber(): string {
-    return this.#officeNumber;
+    return this.#telephoneNumber.number;
   }
 
   set officeNumber(arg: string) {
-    this.#officeNumber = arg;
+    this.#telephoneNumber.number = arg;
   }
 }
 
