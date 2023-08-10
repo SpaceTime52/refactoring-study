b7eb7e4838c18b2fd392e37f91692077eb873e87
SpaceTime52 <love.free.work@gmail.com>
2023-08-10 10:20:09 +0900

    feat: 7.3 기본형을 객체로 바꾸기

--- src/ch7/7-3.ts
+++ src/ch7/7-3.ts
@@ -1,16 +1,30 @@
-type Priority = 'normal' | 'high' | 'rush';
+// enum 을 활용해서 우선순위 로직을 개선
+enum PriorityLevel {
+  NORMAL = 0,
+  HIGH = 1,
+  RUSH = 2,
+}
 
 class Order {
-  priority: Priority;
+  priority: PriorityLevel;
 
-  constructor(data: { priority: Priority }) {
+  constructor(data: { priority: PriorityLevel }) {
     this.priority = data.priority;
+  }
+
+  // 우선순위 로직을 Order 안으로 이동
+  isHighPriority(): boolean {
+    return this.priority >= PriorityLevel.HIGH;
   }
 }
 
-const orders: Order[] = [new Order({ priority: 'normal' }), new Order({ priority: 'high' }), new Order({ priority: 'rush' })];
+const orders: Order[] = [
+  new Order({ priority: PriorityLevel.NORMAL }),
+  new Order({ priority: PriorityLevel.HIGH }),
+  new Order({ priority: PriorityLevel.RUSH }),
+];
 
-const highPriorityCount: number = orders.filter((o) => 'high' === o.priority || 'rush' === o.priority).length;
+const highPriorityCount: number = orders.filter((order: Order) => order.isHighPriority()).length;
 
 export function printSevenDashThree(): void {
   console.log('2:', highPriorityCount);
