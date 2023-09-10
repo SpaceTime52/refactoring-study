724c3ad31226220583b4c1c81587daf7e24f0235
SpaceTime52 <love.free.work@gmail.com>
2023-09-10 13:06:32 +0900

    refactor(chapter 11-9): chapter 11-9

--- src/ch11/11-9.ts
+++ src/ch11/11-9.ts
@@ -1,27 +1,43 @@
+// Chapter 11-9. 함수를 명령으로 바꾸기 커맨드패턴
 import { Candidate, MedicalExam } from './chapter-11.types';
 
-export function score(
-  candidate: Candidate,
-  medicalExam: MedicalExam,
-  scoringGuide: ScoringGuide,
-): number {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+// 명령 객체
+export class ScoreCalculator {
+  #candidate: Candidate;
+  #medicalExam: MedicalExam;
+  #scoringGuide: ScoringGuide;
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+  constructor(
+    candidate: Candidate,
+    medicalExam: MedicalExam,
+    scoringGuide: ScoringGuide,
+  ) {
+    this.#candidate = candidate;
+    this.#medicalExam = medicalExam;
+    this.#scoringGuide = scoringGuide;
   }
 
-  let certificationGrade: 'regular' | 'low' = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
+  execute(): number {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.#medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+
+    let certificationGrade: 'regular' | 'low' = 'regular';
+    if (
+      this.#scoringGuide.stateWithLowCertification(this.#candidate.originState)
+    ) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
 
 export class ScoringGuide {
@@ -29,3 +45,15 @@ export class ScoringGuide {
     return state < 5;
   }
 }
+
+// 사용
+const candidate = { originState: 1 };
+const medicalExam = { isSmoker: true };
+const scoringGuide = new ScoringGuide();
+const scoreCalculator = new ScoreCalculator(
+  candidate,
+  medicalExam,
+  scoringGuide,
+);
+
+console.log(scoreCalculator.execute()); // 실행 결과: 5
