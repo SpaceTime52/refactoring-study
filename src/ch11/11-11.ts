216764877eae8bf7ad65236f26d4cd8aa913d637
SpaceTime52 <love.free.work@gmail.com>
2023-09-10 13:17:02 +0900

    refactor: chapter 11-11

--- src/ch11/11-11.ts
+++ src/ch11/11-11.ts
@@ -1,15 +1,16 @@
+// Chapter 11-11. 수정된 값 반환하기
 import { Point } from './chapter-11.types';
 
 export function ascentVelocity(points: Point[], totalMinutes: number): number {
-  function calculateAscent(): void {
+  function calculateAscent(): number {
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
       const verticalChange = points[i].elevation - points[i - 1].elevation;
-      totalAscent += verticalChange > 0 ? verticalChange : 0;
+      result += verticalChange > 0 ? verticalChange : 0;
     }
+    return result;
   }
 
-  let totalAscent = 0;
-  calculateAscent();
-
+  let totalAscent = calculateAscent();
   return totalAscent / totalMinutes;
 }
