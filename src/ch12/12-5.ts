77a3c44a32bb1de7ff20a1cd594e2dffbc415850
SpaceTime52 <love.free.work@gmail.com>
2023-09-20 08:35:24 +0900

    chore: 준비

--- src/ch12/12-5.ts
+++ src/ch12/12-5.ts
@@ -1,11 +1,7 @@
 class Employee {
-  protected quota?: any;
+  #quota?: any;
 }
 
-class Engineer extends Employee {
-  // If specific properties or methods for Engineer are required, they can be added here
-}
+class Engineer extends Employee {}
 
-class Salesperson extends Employee {
-  // If specific properties or methods for Salesperson are required, they can be added here
-}
+class Salesperson extends Employee {}
