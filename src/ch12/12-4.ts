606e4966e6ac78c46f5ee453c9458249f64fc7cc
SpaceTime52 <love.free.work@gmail.com>
2023-09-20 08:31:56 +0900

    chore: 준비

--- src/ch12/12-4.ts
+++ src/ch12/12-4.ts
@@ -1,20 +1,7 @@
 abstract class Employee {
-  // Define an abstract getter called `quota` without an implementation.
-  abstract get quota(): any;
+  get quota(): any;
 }
 
-class Engineer extends Employee {
-  // Implement the abstract `quota` getter in the derived class, if needed.
-  get quota(): any {
-    // Add the necessary logic here
-    return undefined; // Placeholder, update with the actual logic.
-  }
-}
+class Engineer extends Employee {}
 
-class Salesperson extends Employee {
-  // Implement the abstract `quota` getter in the derived class, if needed.
-  get quota(): any {
-    // Add the necessary logic here
-    return undefined; // Placeholder, update with the actual logic.
-  }
-}
+class Salesperson extends Employee {}
